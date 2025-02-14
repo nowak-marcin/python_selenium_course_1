a0a592f691bb19c28bd295080f2bdcca6a234350
nowakmarcin2204 <nowakmarcin2204@gmail.com>
2025-02-14 16:04:08 +0100

    for loop - new info

--- python_section/loops/for_loop.py
+++ python_section/loops/for_loop.py
@@ -18,3 +18,10 @@ for i in my_text.split():
     if len(i) <= 3:
         small_words.append(i)
 print(small_words)
+
+
+my_list = [['india', 'delhi'], ['germany', 'berlin'], ['norway', 'oslo']]
+my_dict = dict(my_list)
+print(my_dict)
+for country, city in my_dict.items():
+    print(country, city)
--- python_section/loops/for_loop_range.py
+++ python_section/loops/for_loop_range.py
@@ -6,8 +6,14 @@ for i in range(2,4):
     print(i)
     print('-------')
 
+
 my_lista = []
 for i in range(5):
     my_lista.append('abc')
 print(my_lista)
 print(len(my_lista))
+
+
+a_text = ['Mary', 'had', 'a', 'little', 'lamb']
+for lp in range(len(a_text)):
+    print(lp, a_text[lp])
