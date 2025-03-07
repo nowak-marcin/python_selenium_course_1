817b9bcace6b4989ed9f43f19f82f94491f8b68e
nowakmarcin2204 <nowakmarcin2204@gmail.com>
2025-03-07 13:12:29 +0100

    test dummy rename

--- python_selenium_course_1/tests/test_dummy.py
+++ python_selenium_course_1/tests/test_dummy.py
@@ -1,9 +1,17 @@
+import time
 import pytest
+from python_selenium_course_1.conftest import init_driver
+from python_selenium_course_1.src.helpers.config_helpers import get_base_url
 
 
 @pytest.mark.usefixtures("init_driver")
-class TestDummy():
+class TestDummy:
 
     def test_dummy_func(self):
+        url = get_base_url()
+        self.driver.get(url)
+        time.sleep(5)
+        assert 'Demo eCom Store' in self.driver.title, 'incorrect page title'
+        print(self.driver.title)
         print('test kontrolny 1')
         print('test kontrolny 2')
